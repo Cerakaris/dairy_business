d0e32b5023b411c27ef0e467af85ef104aae6c1c
JACKLINEKARIUKI <jackline.kariuki@student.moringaschool.com>
2022-07-17 05:30:06 +0300

    modify the function for getting monthly income

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
 // let shedName, shedProduce, sheds, shedsList, dailyShedProduce, totalProduceDisplay, totalDailyProduce, sellingPrice,weeklyIncomeDisplay, weeklyIncome, monthlyProduceDisplay, monthlyIncomeDisplay, yearlyIncomeDisplay ;
-let shedName, shedProduce, sheds, shedsList, dailyShedProduce, totalProduceDisplay, totalDailyProduce, sellingPrice,weeklyIncomeDisplay;
+let shedName, shedProduce, sheds, shedsList, dailyShedProduce, totalProduceDisplay, totalDailyProduce, sellingPrice,weeklyIncomeDisplay, weeklyIncome, monthlyProduceDisplay, monthlyIncomeDisplay;
 
-// let months = [
-// 	{name:'January',days: 31},
-// 	{name:'February', days: 29},
-// 	{name:'March', days: 31},
-// 	{name:'April',days: 30},
-// 	{name:'May', days: 31},
-// 	{name:'June',days: 30},
-// 	{name:'July', days: 31},
-// 	{name:'August', days: 31},
-// 	{name:'September',days: 30},
-// 	{name:'October', days: 31},
-// 	{name:'November', days: 30},
-// 	{name:'December', days: 31},
-// ];
+let months = [
+	{name:'January',days: 31},
+	{name:'February', days: 29},
+	{name:'March', days: 31},
+	{name:'April',days: 30},
+	{name:'May', days: 31},
+	{name:'June',days: 30},
+	{name:'July', days: 31},
+	{name:'August', days: 31},
+	{name:'September',days: 30},
+	{name:'October', days: 31},
+	{name:'November', days: 30},
+	{name:'December', days: 31},
+];
 
 shedName = document.getElementById("shedNameInput");
 shedProduce = document.getElementById("shedProduceInput");
