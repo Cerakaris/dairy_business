0ca15fc2e6760ecd760dad7106e4b527577d95a5
JACKLINEKARIUKI <jackline.kariuki@student.moringaschool.com>
2022-07-17 05:20:26 +0300

    create funstions to collect user input and store in array

--- index.js
+++ index.js
@@ -1,66 +1,105 @@
 
-// let noOflitres = 50;
+// let shedName, shedProduce, sheds, shedsList, dailyShedProduce, totalProduceDisplay, totalDailyProduce, sellingPrice,weeklyIncomeDisplay, weeklyIncome, monthlyProduceDisplay, monthlyIncomeDisplay, yearlyIncomeDisplay ;
+let shedName, shedProduce, sheds, shedsList;
 
-// let sellingPrice = 45;
+// let months = [
+// 	{name:'January',days: 31},
+// 	{name:'February', days: 29},
+// 	{name:'March', days: 31},
+// 	{name:'April',days: 30},
+// 	{name:'May', days: 31},
+// 	{name:'June',days: 30},
+// 	{name:'July', days: 31},
+// 	{name:'August', days: 31},
+// 	{name:'September',days: 30},
+// 	{name:'October', days: 31},
+// 	{name:'November', days: 30},
+// 	{name:'December', days: 31},
+// ];
 
-// var income;
+shedName = document.getElementById("shedNameInput");
+shedProduce = document.getElementById("shedProduceInput");
+shedsList = document.getElementById("shedsList");
+// dailyShedProduce = document.getElementById("dailyShedProduce");
+// totalProduceDisplay = document.getElementById("totalProduceDisplay");
+// yearlyIncomeDisplay = document.getElementById("yearlyIncomeDisplay");
+// monthlyIncomeDisplay = document.getElementById("monthlyIncomeDisplay");
+// sellingPrice = document.getElementById("sellingPrice");
+// weeklyIncomeDisplay = document.getElementById("weeklyIncome");
 
-// function income(selling,noOfDays){
-//     var x = selling * noOfDays * noOflitres;
-//     return x;
+sheds = [];
+
+function stringifySheds(){
+	let looped = '';
+    dailyProduce = '';
+	for (let i = 0; i < sheds.length; i++) {
+		 looped += `<div> <p>Shed name : ${sheds[i].name} ,  Milk in Litres : ${sheds[i].produce}</p></div>`
+	};
+	return looped;
+};
+
+function addRecord() {
+		sheds.push({name: shedName.value, produce: shedProduce.value});
+		shedsList.innerHTML = stringifySheds();
+        shedName.value = "";
+        shedProduce.value = "";	
+};
+
+// function stringifyShedProduce(){
+// 	let looped = '';
+//     dailyProduce = '';
+// 	for (let i = 0; i < sheds.length; i++) {
+//         looped += `<div> <p> Your production in ${sheds[i].name} is ${sheds[i].produce} Litres per day. </p></div>`
+// 	};
+// 	return looped;
+// };
+
+function dailyProducePerShed() {
+    // sheds.push({name: shedName.value, produce: shedProduce.value});
+    dailyShedProduce.innerHTML = stringifyShedProduce();
+    
+};
+
+
+// function getTotalDailyProduce() {
+//     let total = 0;
+// 	for (let i = 0; i < sheds.length; i++) {
+// 		total += Number(sheds[i].produce)
+// 	};
+//     return total;
+// }
+
+// function totalProduce(){
+// 	totalDailyProduce = getTotalDailyProduce();
+// 	totalProduceDisplay.innerHTML = `The total production is ${totalDailyProduce} litres per day`;
+// };
+
+// function totalWeeklyIncome(){
+	
+// 	weeklyIncome = getTotalDailyProduce() * Number(sellingPrice.value) * 7;
+// 	weeklyIncomeDisplay.innerHTML = `Your weekly income will be Ksh. ${weeklyIncome}`;
 // };
 
 
+// function monthlyIncome() {
+// 	let monthlyProduce = months.slice();
+// 	let monthlyIncomeHtml = '';
+    
+// 	for (let i = 0; i < monthlyProduce.length; i++) {
+// 		monthlyProduce[i].produce = monthlyProduce[i].days * Number(totalDailyProduce) ;
+//         monthlyProduce[i].income = monthlyProduce[i].days * Number(totalDailyProduce)*Number(sellingPrice.value); ;
+// 	};
+// 	for (let i = 0; i < monthlyProduce.length; i++) {
+//         monthlyIncomeHtml += `<div> Your income for ${monthlyProduce[i].name} is Ksh. ${monthlyProduce[i].income}</div>`
+// 	};
+// 	monthlyIncomeDisplay.innerHTML = monthlyIncomeHtml;
+// };
 
-// console.log(`Your weekly income will be ${   income(sellingPrice,7)   }`);
-// console.log(`Your yearly income will be ${   income(sellingPrice,366)   }`);
 
-
-// const months = {
-//     january : 31,
-//     February : 29,
-//     March:31,
-//     December : 31
-// }
-
-// for (const x in months){
-//     console.log(`Your income for ${x} is ${income(sellingPrice, months[x])}`)
-// }
-
-// var shedA, shedB, shedC, shedD, shedE, shedF;
-// var sellingPrice = 45
-
-// var litresPerShed = [shedA, shedB, shedC, shedD, shedE, shedF];
-// var farmSheds = ["Shed A", "Shed B", "Shed C", "Shed D", "Shed E", "Shed F"];
-
-// var totalProduction = function (litresPerShed){
-//     var total = 0;
-//     for (var i = 0; i<litresPerShed.length; i++){
-//         total += litresPerShed[i];
-//         // return x;
-//         console.log(`Your production in ${farmSheds[i]} is ${litresPerShed[i]} litres per day`);
-//     }
-//     console.log(`The total production is ${total} litres per day`);
-// }
-
-// totalProduction();
-
-var shedName = function (name)
-{
-    var shedNameAdd = document.getElementById("add");
-    var shedNameInput = document.getElementById("inputnumber2");
-    shedNameAdd.onclick(() => console.log(shedNameInput.value));
-    var shedNameArray= [];
-    shedNameArray.push(shedNameAdd.value);
-    shedNameArray.push(name);
-    return shedNameArray;
-}
-
-// console.log(shedName('shedB'));
-var shedNameInput = document.getElementById("inputnumber2");
-function addRecord(e) {
-    console.log('clicked')
-    console.log(shedNameInput.value);
-}
-
-console.log('maneno')
+// function yearlyIncome() {
+// 	let totalYearly = 0 ;
+// 	for (let i = 0; i < months.length; i++) {
+// 		totalYearly += months[i].days * Number(totalDailyProduce)*Number(sellingPrice.value);
+// 	};
+// 	yearlyIncomeDisplay.innerHTML = `Your yearly produce = ${totalYearly}`;
+// };
